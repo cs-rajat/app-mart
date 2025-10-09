92fe68a8cdfd6b731fcd22ecb47f77dbc5293c57
Rahul <cs.rajat75@gmail.com>
2025-10-09 11:05:34 +0600

    app card setup

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React from "react";
+import downIcon from "../assets/icon-downloads.png";
+import ratIcon from "../assets/icon-ratings.png";
 
-const ProductCard = ({product}) => {
+const ProductCard = ({ product }) => {
+  const { title, image, downloads, ratingAvg } = product;
+  return (
+    <div
+      className="card p-5 rounded-md flex flex-col gap-2 shadow-xs bg-white 
+    transition-all duration-300 hover:shadow-lg hover:-translate-y-3 cursor-pointer"
+    >
+      <img
+        className="bg-gray-300 p-3 rounded-md"
+        src={image}
+        alt={product.imageAlt}
+        onError={(e) => (e.currentTarget.src = product.imageFallback)}
+      />
 
-    const {title,image,downloads,ratingAvg}=product
-    return (
-        <div className='border p-5 rounded-md flex flex-col gap-2'>
-            <img src={image}></img>
-            <h1>{title}</h1>
-            <div className=' flex justify-between'>
-                <p>{downloads}</p>
-                <p>{ratingAvg}</p>
-            </div>
+      <h1 className="font-semibold">{title}</h1>
 
-        </div>
-    );
+      <div className="flex justify-between">
+        <p className="flex btn text-green-500">
+          <img className="w-5" src={downIcon} alt="downloads" />
+          {downloads}
+        </p>
+        <p className="flex btn text-yellow-500">
+          <img className="w-5" src={ratIcon} alt="rating" />
+          {ratingAvg}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,12 +6,21 @@ const Home = () => {
     const products = useLoaderData()
     console.log(products)
     return (
-        <div className='grid grid-cols-4 gap-5 '>
+       <section className='bg-gray-100'>
+         <div className='max-w-11/12 mx-auto py-10'>
+            <div className='text-center  space-y-2'>
+                <h1 className='text-3xl font-bold'>Trending Apps</h1>
+                <p className='text-gray-500'>Explore All Trending Apps on the Market developed by us</p>
+            </div>
+             <div className='grid grid-cols-4 gap-10 mt-10'>
             
             {
                 products.map(product=><ProductCard product={product}></ProductCard>)
             }
+              </div>
         </div>
+       
+       </section>
     );
 };
 
